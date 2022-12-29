318f03d8525e8fdd654aa809281e721dff926c12
rafischer1 <artiefischer@gmail.com>
2022-12-28 21:34:35 -0500

    fotmob attempt

--- kit-app/src/stores.ts
+++ kit-app/src/stores.ts
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store';
 
+
 export const games = writable([{id: 1, teamHome: "Real Madrid",
     teamAway: "Real Betis", minute: 0, scoreHome: 0, scoreAway: 0, live: false},
     {id: 2, teamHome: "Barcelona", teamAway: "Español",
